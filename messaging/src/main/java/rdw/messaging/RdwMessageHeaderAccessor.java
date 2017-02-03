79f6f670480b94a5363feb2b26934930dfff0710
malaffoon <laffoon@san.rr.com>
2017-02-03 15:10:15 -0800

    missed a few files in initial checkin

--- messaging/src/main/java/rdw/messaging/RdwMessageHeaderAccessor.java
+++ messaging/src/main/java/rdw/messaging/RdwMessageHeaderAccessor.java
@@ -1,7 +1,39 @@
 package rdw.messaging;
 
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+
 /**
- * Created by marklaffoon on 2/3/17.
+ * A {@link org.springframework.messaging.support.MessageHeaderAccessor}
+ * implementation giving access to RDW-specific headers.<br/>
+ *
+ * In theory, this could be refactored so each step of the ingest pipeline has its own set of
+ * headers and accessor. However, this approach is sufficient for now.
  */
-public class RdwMessageHeaderAccessor {
+public class RdwMessageHeaderAccessor extends MessageHeaderAccessor {
+
+    /**
+     * Wrap a message's headers.
+     *
+     * @param message message to extract headers from
+     * @return a new RdwMessageHeaderAccessor
+     */
+    public static RdwMessageHeaderAccessor wrap(Message<?> message) {
+        return new RdwMessageHeaderAccessor(message);
+    }
+
+
+    public RdwMessageHeaderAccessor setContent(final String content) {
+        setHeader(RdwMessageHeaders.Content, content);
+        return this;
+    }
+
+    public String getContent() {
+        return (String) getHeader(RdwMessageHeaders.Content);
+    }
+
+
+    private RdwMessageHeaderAccessor(final Message<?> message) {
+        super(message);
+    }
 }
--- messaging/src/main/java/rdw/messaging/RdwMessageHeaders.java
+++ messaging/src/main/java/rdw/messaging/RdwMessageHeaders.java
@@ -1,7 +1,16 @@
 package rdw.messaging;
 
+import org.springframework.messaging.MessageHeaders;
+
 /**
- * Created by marklaffoon on 2/3/17.
+ * Pre-defined names and prefixes to be used for setting and/or retrieving RDW message properties
+ * from/to {@link org.springframework.messaging.Message Message} Headers.<br/>
  */
 public class RdwMessageHeaders {
+
+    public static final String ContentType = MessageHeaders.CONTENT_TYPE;
+
+    public static final String Prefix = "rdw_";
+
+    public static final String Content = Prefix + "content";    // TRT_V0, StudentReg, etc.
 }
