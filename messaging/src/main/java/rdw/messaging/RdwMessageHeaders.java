1f81b0b426ca72a789006cd3b59348f79657cbb5
malaffoon <laffoon@san.rr.com>
2017-02-08 10:55:40 -0800

    comment about header key naming convention

--- messaging/src/main/java/rdw/messaging/RdwMessageHeaders.java
+++ messaging/src/main/java/rdw/messaging/RdwMessageHeaders.java
@@ -5,6 +5,11 @@ import org.springframework.messaging.MessageHeaders;
 /**
  * Pre-defined names and prefixes to be used for setting and/or retrieving RDW message properties
  * from/to {@link org.springframework.messaging.Message Message} Headers.<br/>
+ * <p>
+ * The naming convention uses prefixes to scope a header. Thus, all these will have "rdw_" as the
+ * prefix. Additional prefixes may be used to further scope a header. The header key itself should
+ * be camel-cased. For example, rdw_attr_shoeSize.
+ * </p>
  */
 public class RdwMessageHeaders {
 
